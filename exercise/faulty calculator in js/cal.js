1860b98efba348999ede616048845b90185ca1e3
Ajay Yadav <ayajay2006@gmail.com>
2026-01-23 09:35:15 +0530

    some new changes

---
+++ exercise/faulty calculator in js/cal.js
@@ -0,0 +1,20 @@
+let a=17;
+let b=2;
+
+let c=Math.random()
+
+console.log(c)
+
+if (c<=0.1) {
+    console.log("sum is :",a-b)
+    console.log("substract is :",a/b)
+    console.log("multiply is :",a+b)
+    console.log("division is :",a**b)
+}
+
+else{
+     console.log("sum is :",a+b)
+    console.log("substract is :",a-b)
+    console.log("multiply is :",a*b)
+    console.log("division is :",a/b)
+}
