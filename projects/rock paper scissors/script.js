77383a778eff4380969c06ef0192e6ec267f040b
Ajay Yadav <ayajay2006@gmail.com>
2026-06-14 14:51:29 +0530

    retry function is left yet

--- projects/rock paper scissors/script.js
+++ projects/rock paper scissors/script.js
@@ -1,69 +1,90 @@
 let systemChoice
-let result = 0
+let result
 let choice
 
 const random = () => {
     systemChoice = Math.ceil(2 * Math.random())
+    console.log(systemChoice);
+    systemChoiceText(systemChoice)
 }
 
 const FinalResult = (userChoice) => {
-    if (a == systemChoice) {
+    if (userChoice == systemChoice) {
         result = 1
     }
+    console.log("result", result);
 
 }
 
 const systemChoiceText = async (systemChoice) => {
     await systemChoice
     if (systemChoice == 0) {
-        return "Rock"
+        systemChoiceTextShow.innerText = "Rock"
     }
 
     if (systemChoice == 1) {
-        return "Paper"
+        systemChoiceTextShow.innerText = "Paper"
     }
 
     if (systemChoice == 2) {
-        return "Scissors"
+        systemChoiceTextShow.innerText = "Scissors"
     }
 
 }
 
 const finalResultShow = (result) => {
     if (result == 1) {
-        document.getElementsByClassName("")[0].innerText = "You Win"
+        document.getElementsByClassName("final_result")[0].innerText = "You Win"
+        document.getElementsByClassName("final_result")[0].style.color = "rgb(0, 255, 0)"
     }
     else {
-        document.getElementsByClassName("")[0].innerText = "You Loose"
+        document.getElementsByClassName("final_result")[0].innerText = "You Loose"
+        document.getElementsByClassName("final_result")[0].style.color = "rgb(255, 0, 0)"
+
     }
+}
+
+const caller = (inputchoice) => {
+    choice = inputchoice
+    random()
+    console.log("user", choice)
+    FinalResult(choice)
+    finalResultShow(result)
+    document.getElementsByClassName("result")[0].style.display = "block"
+}
+
+const retry = () => {
+  console.log("retry");
+  
 }
 
 let playbtn = document.getElementsByClassName("play")[0]
 let rockbtn = document.getElementsByClassName("rock")[0]
 let paperbtn = document.getElementsByClassName("paper")[0]
-let scessorbtn = document.getElementsByClassName("scissors")[0]
+let scessorbtn = document.getElementsByClassName("scessors")[0]
+let retrybtn = document.getElementsByClassName("retry")[0]
 
 let show = document.getElementsByClassName("show")[0]
+
+let systemChoiceTextShow = document.getElementsByClassName("answer")[0]
 
 playbtn.addEventListener("click", () => {
     show.style.display = "block"
 })
 
 rockbtn.addEventListener("click", () => {
-    choice = 0
-    finalResultShow(result)
-    random()
+    caller(0)
+
 })
 paperbtn.addEventListener("click", () => {
-    choice = 1
-    finalResultShow(result)
-    random()
+    caller(1)
+
 })
+
 scessorbtn.addEventListener("click", () => {
-    choice = 2
-    finalResultShow(result)
-    random()
+    caller(2)
+
 })
 
-
+retrybtn.addEventListener("click", retry)
 
